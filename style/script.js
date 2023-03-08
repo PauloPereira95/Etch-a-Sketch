553fdd49df8bcc86ad13207ec310d597615ca682
PauloPereira95 <paulohuawei95@gmail.com>
2023-03-08 22:19:04 +0000

    base feita , vereficar input utilizador

--- style/script.js
+++ style/script.js
@@ -1,12 +1,52 @@
 const grid = document.querySelector('.grid');
+const resetGrid = document.querySelector('.reset');
+const btnChangeSize = document.querySelector('.btnChangeSize');
+let squares = [];
+let user,userSquares;
 
-function makeGrid(rows,cols) {
-    grid.style.setProperty('--grid-rows',rows);
-    grid.style.setProperty('--grid-cols',cols);
-    for (let c = 0; c < (rows* cols); c++) {
-        let cel = document.createElement('div');
-        grid.appendChild(cel).className ="grid-item";
-        
+function makeGrid(rows, cols) {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', cols);
+    for (let c = 0; c < (rows * cols); c++) {
+        let square = document.createElement('div');
+        square.classList.add('grid-item');
+        squares.push(square);
+        grid.appendChild(square);
+
     };
 };
-makeGrid(16,16);
+
+grid.addEventListener('mouseover', function (e) {
+    if (e.target.classList.contains('grid-item')) {
+        e.target.style.backgroundColor = 'blue';
+    }
+
+
+})
+grid.addEventListener('mouseleave', function (e) {
+    grid.style.backgroundColor = 'white';
+
+});
+function askSize() {
+     do {
+        user = Number(prompt('Number of squares per side'));
+        console.log(user);
+    } while (user > 100 || user === 'NaN' || user === 'undefined');
+    return user;
+    
+}
+resetGrid.addEventListener('click', () => {
+    squares.forEach(squares=> {
+        grid.removeChild(squares);3
+    });
+    squares = [];
+    makeGrid(16,16);
+});
+btnChangeSize.addEventListener('click', () => {
+    userSquares = askSize();
+    makeGrid(userSquares, userSquares);
+     
+});
+userSquares = askSize();
+makeGrid(userSquares, userSquares);
+
