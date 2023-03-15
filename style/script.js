f935a1c1c1fd0ade20b331743ba7f5446f8b560f
PauloPereira95 <paulohuawei95@gmail.com>
2023-03-15 21:40:26 +0000

    função apagar sem alterar o input color

--- style/script.js
+++ style/script.js
@@ -7,34 +7,43 @@ const btnPencil = document.querySelector('.btnPencil');
 
 
 let squares = [];
-let user,userSquares,color;
+let user,userSquares,color,state;
 
-function makeGrid(rows, cols) {
-    grid.style.setProperty('--grid-rows', rows);
-    grid.style.setProperty('--grid-cols', cols);
-    for (let c = 0; c < (rows * cols); c++) {
-        let square = document.createElement('div');
-        square.classList.add('grid-item');
-        squares.push(square);
-        grid.style.maxWidth='700px';
-        grid.style.maxHeigth='700px';
-        grid.appendChild(square);
-
-    };
-};
-
+btnPencil.addEventListener('click', ()=> {
+    state = 'btnPencil';
+});
+inputColor.addEventListener('click' , () => {
+    state = 'inputColor';
+})
 /*btnPencil.addEventListener('click',(e) => {
     color = inputColor.value;
 });*/
 btnClear.addEventListener('click', (e) => {
-    inputColor.value="#ffffff";
+    state = 'clear';
+    //inputColor.value="#ffffff";
 });
 
 grid.addEventListener('mouseover', function (e) {
-    color = inputColor.value; // conflito com btn clear
-    if (e.target.classList.contains('grid-item')) {
-        console.log(inputColor.value);
-        e.target.style.backgroundColor = color;
+    switch (state) {
+        case 'clear':
+            e.target.style.backgroundColor ="#ffffff";
+            break;
+            case 'inputColor':
+                color = inputColor.value; // conflito com btn clear
+                if (e.target.classList.contains('grid-item')) {
+                    console.log(inputColor.value);
+                    e.target.style.backgroundColor = inputColor.value;
+                }
+                break;
+            case 'btnPencil':
+                if (e.target.classList.contains('grid-item')) {
+                    console.log(inputColor.value);
+                    e.target.style.backgroundColor = inputColor.value;
+                }
+            break;
+        default:
+            console.log('erro!');
+            break;
     }
 
 
@@ -43,12 +52,6 @@ grid.addEventListener('mouseleave', function () {
     grid.style.backgroundColor = 'white';
 
 });
-function askSize() {
-    do {
-        user = Number(prompt('Number of squares per side:     (MAX: 55)'));
-    } while (user > 55 || user === 0 || isNaN(user));
-    return user;
-}
 resetGrid.addEventListener('click', () => {
     squares.forEach(squares=> {
         grid.removeChild(squares);3
@@ -65,6 +68,32 @@ btnChangeSize.addEventListener('click', () => {
     makeGrid(userSquares, userSquares);
      
 });
+function rndRgb() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    
+}
+function askSize() {
+    do {
+        user = Number(prompt('Number of squares per side:     (MAX: 55)'));
+    } while (user > 55 || user === 0 || isNaN(user));
+    return user;
+}
+function makeGrid(rows, cols) {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', cols);
+    for (let c = 0; c < (rows * cols); c++) {
+        let square = document.createElement('div');
+        square.classList.add('grid-item');
+        squares.push(square);
+        grid.style.maxWidth='700px';
+        grid.style.maxHeigth='700px';
+        grid.appendChild(square);
+
+    };
+};
 userSquares = askSize();
 makeGrid(userSquares, userSquares);
 
